4ce487b8b2d90cb09c505c37e9a93fbdcfd73b9a
Christina Deming <christinamdeming@gmail.com>
2020-08-20 10:00:57 -0400

    Set up ejs, bp, & mysql, treat home.ejs like html - you will need to fill in your own info on the mysql connection block, and remove that info before pushing code when testing on a local database.

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const express =  require("express")
-const app = express()
+const express =  require("express");
+const app = express();
 const PORT = process.env.PORT || 5000
+const mysql = require('mysql');
+const bodyParser  = require("body-parser");
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+
+let connection = mysql.createConnection({
+    host     : '',
+    user     : '',
+    database : '',
+    password : ''
+});
+
 
 app.get("/", (req,res) => {
-    res.send("test")
+    res.render("home")
 });
 
 
