f84fe7de20ef91fec6138bd8dc6af1a08f015d23
christine.scala22@gmail.com <christine.scala22@gmail.com>
2020-08-21 17:03:33 -0400

    remove UN PW

--- cloud9DB.sql
+++ cloud9DB.sql
@@ -1,5 +1,5 @@
 USE cloud9;
-CREATE TABLE customers (fname varchar(30), lname varchar(45), phone varchar(15), user_name varchar(20), password varchar(16));
+CREATE TABLE customers (fname varchar(30), lname varchar(45), phone varchar(15);
 INSERT INTO customers (fname, lname, phone) VALUES ('Johnny', 'Mack', '8675309');
 show tables;
 SELECT * FROM customers;
