0058f71bd47b06803b22876bb54d069368d1c29b
Christina Deming <christinamdeming@gmail.com>
2020-08-23 17:20:16 -0400

    Regexes suck so hard

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ app.use(bodyParser.urlencoded( { extended: true } ));
 app.use(express.static(__dirname + "/public"));
 
 let connection = mysql.createConnection({
-    host     : '',
-    user     : '',
-    database : '',
-    password : ''
+    host     : 'localhost',
+    user     : 'root',
+    database : 'cloud9',
+    password : 'root'
 });
 
 
@@ -30,34 +30,48 @@ app.post("/register", [
         // Remove excess whitespace so we can see if we got only spaces
         .trim()
         // Name can't be just empty
-        .not().isEmpty().withMessage("First name is required.")
-        // Matches letters spaces hyphens and apostrophes - there is an enhancement waiting to be added to validator.js that will add this to isAlpha but it isn't done yet
-        .matches(/^[A-Za-z\s'-]+$/).withMessage("Names may only contain letters with spaces, hyphens, and apostrophes.")
+        .notEmpty().withMessage("First name is required.")
+        // If err, kick the user out to fix it
+        .bail()
+        // Matches letters spaces hyphens and apostrophes, including unicode characters for people with accents in their names, see https://regex101.com/r/ZKZkOC/4/ for examples
+        .matches(/^(([ ]*)|([\p{L}]+))([\p{L}]+)([\p{L} '-]+)$/gmiu).withMessage("First name should start with a letter, and may only contain letters with spaces, hyphens, and apostrophes.")
+        // If err, kick the user out to fix it
+        .bail()
         // Match the length of the database column
         .isLength( { min:2, max:30 }).withMessage("Please enter a first name between 2 and 30 characters."),
     check("lname")
         // Same as before but it can be a little longer
         .trim()
-        .not().isEmpty().withMessage("Last name is required.")
-        .matches(/^[A-Za-z\s'-]+$/).withMessage("Names may only contain letters with spaces, hyphens, and apostrophes.")
+        .notEmpty().withMessage("Last name is required.")
+        .bail()
+        .matches(/^(([ ]*)|([\p{L}]+))([\p{L}]+)([\p{L} '-]+)$/gmiu).withMessage("Last name should start with a letter, and may only contain letters with spaces, hyphens, and apostrophes.")
+        .bail()
         .isLength( { min:2, max:45 }).withMessage("Please enter a last name between 2 and 45 characters."),
     check("phoneNum1")
         // Phone number can't just be empty
-        .not().isEmpty().withMessage("Phone number is required.")
+        .notEmpty().withMessage("Phone number is required.")
+        .bail()
         // Phone number must be numbers
         .isInt().withMessage("Phone numbers may only contain numbers.")
+        .bail()
         // Must match the length of the part of the phone number
-        .isLength( { min:3, max:3 }).withMessage("Please enter a ten digit phone number.")
-        // If err, bail out here and send error message (because all phone number length errors are the same, next block bails as well)
-        .bail(),
+        .isLength( { min:3, max:3 } ).withMessage("Please enter a ten digit phone number."),
     check("phoneNum2")
-        .not().isEmpty().withMessage("Phone number is required.")
+        // Only run this if the first set worked
+        .if(check("phoneNum1").notEmpty().isInt().isLength( { min:3, max:3 } ))
+        .notEmpty().withMessage("Phone number is required.")
+        .bail()
         .isInt().withMessage("Phone numbers may only contain numbers.")
-        .isLength( { min:3, max:3 }).withMessage("Please enter a ten digit phone number.")
-        .bail(),
+        .bail()
+        .isLength( { min:3, max:3 }).withMessage("Please enter a ten digit phone number."),
     check("phoneNum3")
-        .not().isEmpty().withMessage("Phone number is required.")
+        // Only run this if the second set worked which doesn't run if the first set didn't, theoretically
+        // I probably made this harder on myself by suggesting to split the phone number fields
+        .if(check("phoneNum2").notEmpty().isInt().isLength( { min:3, max:3 } ))
+        .notEmpty().withMessage("Phone number is required.")
+        .bail()
         .isInt().withMessage("Phone numbers may only contain numbers.")
+        .bail()
         .isLength( { min:4, max:4 }).withMessage("Please enter a ten digit phone number."),
     ], 
     (req, res) => {  
@@ -81,13 +95,13 @@ app.post("/register", [
         let lname = req.body.lname.trim();
         let phone = req.body.phoneNum1+req.body.phoneNum2+req.body.phoneNum3;
         // Then template out the query, ?? for column names, ? for values in those columns
-        let insert = `INSERT INTO cloud9(??, ??, ??) VALUES (?, ?, ?)`
+        let insert = `INSERT INTO customers(??, ??, ??) VALUES (?, ?, ?)`
         // Actually connect to the database, array is in order of ?s
-        connection.query(insert, ["fname", "lname", "phone", fname, lname, phone], (req, res) => {
+        connection.query(insert, ["fname", "lname", "phone", fname, lname, phone], (err, results) => {
             // If it doesn't work, pitch a fit
             if (err) throw err;
             // Build success message
-            let success = `Thank you for registering, ${fname} ${lname}!  We'll text you coupon codes at ${req.body.phoneNum1}) ${req.body.phoneNum2}-${req.body.phoneNum3}.`
+            let success = `Thank you for registering, ${fname} ${lname}!  We'll text you coupon codes at (${req.body.phoneNum1}) ${req.body.phoneNum2}-${req.body.phoneNum3}.`
             // Send success message - consider making a success page or hiding the form on success
             res.render("home", { success });
         });
