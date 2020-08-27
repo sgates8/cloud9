eac33333e8321a20ae2117b115efb864f1d67e68
Christina Deming <christinamdeming@gmail.com>
2020-08-27 17:50:03 -0400

    Added login for admin page with express-session and express-mysql-session, Also logout by destroying session or timeout 5mins, and only show logout button if logged in, upsert for database so name fields get updated on duplicate phone, made the phone column in the database unique for upsert, merged password.js into express app, made password page match site style, small bugfixes in theming for navbar, added class to handle logout button to style (just floats to other side)

--- cloud9DB.sql
+++ cloud9DB.sql
@@ -1,7 +1,5 @@
 Create DATABASE cloud9;
 USE cloud9;
-CREATE TABLE customers (fname varchar(30), lname varchar(45), phone varchar(15));
-INSERT INTO customers (fname, lname, phone) VALUES ('Johnny', 'Mack', '8675309');
-SELECT * FROM customers;
-DESCRIBE customers;
-show tables;
+CREATE TABLE customers (fname varchar(30), lname varchar(45), phone varchar(10) unique not null);
+INSERT INTO customers (fname, lname, phone) VALUES ('Johnny', 'Mack', '5558675309');
+
--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const PORT = process.env.PORT || 5000;
 
 // We need this for backend form validation
 const { check, validationResult } = require("express-validator");
+// This is for session storage because we did a login page anyway
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
 
 // Set the templating engine
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded( { extended: true } ));
-// app.use(bodyParser.json);
 app.use(express.static(__dirname + "/public"));
 
 // Set the mysql connection
@@ -20,6 +22,53 @@ let connection = mysql.createConnection({
     database : 'cloud9',
     password : 'root'
 });
+// And the session store
+let sessionStore = new MySQLStore({
+    // Whether or not to automatically check for and clear expired sessions:
+    clearExpired: true,
+    // How frequently expired sessions will be cleared; milliseconds:
+    checkExpirationInterval: 900000,
+    // The maximum age of a valid session; milliseconds:
+    expiration: 300000,
+    // Whether or not to create the sessions database table, if one does not already exist:
+    createDatabaseTable: true,
+    // Number of connections when creating a connection pool:
+    connectionLimit: 1,
+    // Whether or not to end the database connection when the store is closed.
+    // The default value of this option depends on whether or not a connection was passed to the constructor.
+    // If a connection object is passed to the constructor, the default value for this option is false.
+    endConnectionOnClose: false,
+    // Create the table if it doesn't already exist with these parameters
+    charset: "utf8mb4_bin",
+    schema: {
+        tableName: "sessions",
+        columnNames: {
+            session_id: "session_id",
+            expires: "expires",
+            data: "data"
+        }
+    }
+}, connection);
+
+//Set up the session
+app.use(session({
+    // This is what our session is encryted with
+    secret: "CBC9DreamTeam",
+    // use the store we created
+    store: sessionStore,
+    // Always initialize the session
+    saveUninitialized: false,
+    // And don't re-save it
+    resave: false,
+    // Session expires in 5 minutes
+    maxAge: 300000
+}));
+
+// Make our session available to our locals object
+app.use(function(req, res, next) {
+    res.locals.loggedIn = req.session.loggedIn;
+    next();
+  });
 
 // Render homepage
 app.get("/", (req, res) => {
@@ -28,7 +77,14 @@ app.get("/", (req, res) => {
 
 
 app.get("/password", (req, res) => {
-    res.render("password");
+    // If login token is still set, go to admin
+    if(req.session.loggedIn) {
+        return res.redirect("/admin");
+    }
+    // If login token is not set, enter password
+    else {
+        res.render("password");
+    }
 });
 
 
@@ -107,13 +163,22 @@ app.post("/register", [
         }
         else {
             // If no errors, add information to database
-            // Then template out the query, ?? for column names, ? for values in those columns
-            let insert = `INSERT INTO customers(??, ??, ??) VALUES (?, ?, ?)`;
+            // Then template out the queries, ?? for column names, ? for values in those columns
+            let insert = `INSERT INTO customers(??, ??, ??) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE ?? = ?, ?? = ?, ?? = ?`;
             // Actually connect to the database, array is in order of ?s
-            connection.query(insert, ["fname", "lname", "phone", fname, lname, phone[0]+phone[1]+phone[2]], (err, results) => {
+            connection.query(insert, [
+                // columns
+                "fname", "lname", "phone", 
+                // values
+                fname, lname, phone[0]+phone[1]+phone[2],
+                // updates column/value pair
+                "fname", fname,
+                "lname", lname,
+                "phone", phone[0]+phone[1]+phone[2]
+                ], (err, results) => {
                 // If it doesn't work, pitch a fit
                 if (err) {
-                    throw err;
+                    console.log(err);
                 }
                 else {
                 // Build success message
@@ -125,33 +190,70 @@ app.post("/register", [
         }
 });
 
+// Access to the admin page
+
+app.post("/authenticate", [
+    check("password")
+    // Password has to match string
+    .matches("dude").withMessage("Please enter the correct password.")
+    ],
+    (req, res) => {
+    // Check our results
+    let result = validationResult(req);
+    // Stuff them in an object
+    let errors = result.errors;  
+    // Show me the errors in the console
+    for (let key in errors) {
+        console.log(errors[key].value);
+    }
+    if (!result.isEmpty()) {
+        // If errors, send back to form with errors
+            res.render("password", { errors });
+    }
+    else {
+        // Set the login token
+        req.session.loggedIn = true;
+        // And go to the admin page
+        res.redirect("admin");
+    }
+});
+
 // Getting all of the infomation from customers
-app.get("/admin",(req,res)=>{
-    connection.query('SELECT * FROM customers',(err, rows, fields)=>{
-        if(!err) {
-        res.render("admin",{rows});
+app.get("/admin", (req, res) => {
+    // If login token is true, you're good to go
+    if (req.session.loggedIn) {
+        // Get all the customers
+        connection.query('SELECT * FROM customers', (err, rows) => {
+            // If no error occurred, build the page
+            if(!err) {
+            res.render("admin", { rows });
+            }
+            // If error occurred, direct it to the console log
+            else {
+            console.log(err);
+            }
+        });
+    }
+    else {
+        res.redirect("password");
+    }
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if(err) {
+            return console.log(err);
         }
         else {
-        console.log(err);
+        res.redirect('/');
         }
-    })
+    });
 });
 
 // And we're running
 app.listen(PORT, () => {
     console.log(`server running on ${PORT}`)
 })
-
-/*  I am not sure this is doing anything here except poking the database, we already have if err where we make queries.
-
-connection.connect((err)=>{
-    if(!err) {
-        console.log('connected');
-    } else{
-        console.log('error \n Error: '+ JSON.strinify(err, undefined, 2));
-    }
-}); 
-*/
 
 
 
